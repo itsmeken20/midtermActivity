62c19ac0c46c064afab358cc34103af14de5e5ed
itsmeken20 <123226533+itsmeken20@users.noreply.github.com>
2024-03-16 16:49:13 +0800

    Add files via upload

---
+++ etete/Untitled-1.php
@@ -0,0 +1,20 @@
+<?php
+class Apple {
+ public $taste;
+
+    public $color; 
+
+private $texture;
+
+public function texture($a){
+return $a;
+}
+
+}
+$fruits = new Apple();
+echo $fruits->texture(rough);
+$fruits->color = "blue";
+
+echo $fruits->taste();
+
+echo $fruits->color;
